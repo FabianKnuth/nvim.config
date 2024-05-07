d80d370837356dea129d7086aa0444221d773d39
Fabian Knuth <fabeknuth@gmail.com>
2024-05-07 11:56:12 +0200

    Error with Mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,14 @@
 return {
   {
     "williamboman/mason.nvim",
+    lazy = false,
   config = function()
     require('mason').setup()
   end
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     config = function()
       require('mason-lspconfig').setup({
         ensure_installed = {
